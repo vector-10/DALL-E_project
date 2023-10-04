2119cb672dbca4d6f7754ed427ad0d43d45d852f
Vector-9 <duzieblaise10@gmail.com>
2023-10-04 18:36:44 +0100

    update

---
+++ Dall-E/src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main">
+      <h1>Anyanwu is gay!</h1>
+    </div>
+  );
+}
+
+export default App;
